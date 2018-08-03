f6c5ae236c37df5fd94907207e3631d2cf844b17
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-08-03 19:59:42 +0200

    added anon guard

--- src/app/guards/require-anon.guard.ts
+++ src/app/guards/require-anon.guard.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RequireAnonGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+export class RequireAnonGuardService implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(): Promise<any> {
+    return this.authService.me()
+      .then(user => {
+        if (user) {
+          this.router.navigate(['/profile']);
+          return false;
+        } else {
+          return true;
+        }
+      })
+      .catch(err => console.log(err));
   }
 }
