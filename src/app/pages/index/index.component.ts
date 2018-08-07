6227ec66542b0756d196956f7637f2d8b15d7895
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-08-07 15:31:08 +0200

    heroku again...

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://app-treat-server.herokuapp.com/'
+  apiUrl: 'https://app-treat-server.herokuapp.com/'
 };
