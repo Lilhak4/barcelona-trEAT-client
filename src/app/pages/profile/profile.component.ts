146b347cf9d2bb9d9ce649b1b704368aad2a7650
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-08-08 16:15:03 +0200

    setup profile favorites a bit

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  user: any;
   constructor(private router: Router) { }
 
   ngOnInit() {
