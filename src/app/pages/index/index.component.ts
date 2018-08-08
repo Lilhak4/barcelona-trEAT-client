deb36acdb548add35a9b071d956f73695b3492e5
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-08-08 20:35:06 +0200

    search guard and pass

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // ----Services----
 import { SearchRestaurantsService } from '../../services/search-restaurants.service';
+import { AuthService } from '../../services/auth.service';
 
 
 @Component({
@@ -13,22 +14,29 @@ export class IndexComponent implements OnInit {
   restaurants: any;
   location: any;
   loading: boolean;
+  user: any;
 
-  constructor(private router: Router, private searchRestaurants: SearchRestaurantsService) { }
+  constructor(private router: Router, private authService: AuthService, private searchRestaurants: SearchRestaurantsService) { }
 
   ngOnInit() {
     this.loading = false;
+    this.user = this.authService.getUser();
   }
 
   handleSearch(data, location) {
-    this.loading = true;
-    this.searchRestaurants.getAll(data, location)
-      .then((results: any) => {
-        this.restaurants = results.jsonBody.businesses;
-        console.log(results);
-        this.loading = false;
-      })
-      .catch();
+    if (this.user) {
+      this.loading = true;
+      this.searchRestaurants.getAll(data, location)
+        .then((results: any) => {
+          this.restaurants = results.jsonBody.businesses;
+          console.log(results);
+          this.loading = false;
+        })
+        .catch();
+    } else {
+      this.router.navigate(['/login']);
+    }
+
 
   }
 
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,9 +10,10 @@ import { Router } from '@angular/router';
 export class ProfileComponent implements OnInit {
   user: any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+    this.user = this.authService.getUser();
   }
 
 }
